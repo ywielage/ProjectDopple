d4ab5cdf2193459a72f6bcae2f417652f38ad0c3
Yanniek <yanniek.wielage@student.nhlstenden.com>
2021-05-13 12:29:17 +0200

    Layout gekoppeld met Klasse

---
+++ app/src/main/java/com/inf2c/doppleapp/TestRun/TestRun.java
@@ -0,0 +1,15 @@
+package com.inf2c.doppleapp.TestRun;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.inf2c.doppleapp.R;
+
+public class TestRun extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_run);
+    }
+}
