0722d62a4bc3e636aff1f368e7ec48b8bc362356
Lars <larskuizenga@live.nl>
2021-06-01 16:57:46 +0200

    Changed XMLParser example used

--- app/src/main/java/com/inf2c/doppleapp/TestRun/TestXMLParser.java
+++ app/src/main/java/com/inf2c/doppleapp/TestRun/TestXMLParser.java
@@ -1,145 +1,83 @@
 package com.inf2c.doppleapp.TestRun;
 
-import android.util.Xml;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+import com.inf2c.doppleapp.jsonConversion.models.Track.Track;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 
 public class TestXMLParser {
-    // We don't use namespaces
-    private static final String ns = null;
 
-    public List parse(InputStream in) throws XmlPullParserException, IOException {
-        try {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(in, null);
-            parser.nextTag();
-            return readFeed(parser);
-        } finally {
-            in.close();
-        }
-    }
 
     /*
     Test run code
+
     InputStream object = this.getResources().openRawResource(R.raw.dopple_session_20210511164705_1);
-        TestXMLParser parser = new TestXMLParser();
+    TestXMLParser parser = new TestXMLParser();
 
-        try {
-            System.out.println("==============================");
-            List list = parser.parse(object);
-            System.out.println("==============================" + list.size());
-
-        } catch (XmlPullParserException | IOException e) {
-            e.printStackTrace();
-        }
+    System.out.println("==============================");
+    List list = parser.parse(object);
+    System.out.println("==============================" + list.size());
      */
 
-    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
+    private List<Trackpoint> employees= new ArrayList<Trackpoint>();
+    private Trackpoint employee;
+    private String text;
 
-        parser.require(XmlPullParser.START_TAG, ns, "TrainingCenterDatabase");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            System.out.println("H");
-            String name = parser.getName();
-            // Starts by looking for the entry tag
-            if (name.equals("Trackpoint")) {
-
-                entries.add(readEntry(parser));
-            }
-        }
-        return entries;
-    }
-    public static class Entry {
-        public final String time;
-
-        private Entry(String time) {
-            this.time = time;
-        }
+    public List<Trackpoint> getEmployees() {
+        return employees;
     }
 
-    // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them off
-    // to their respective "read" methods for processing. Otherwise, skips the tag.
-    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, ns, "Trackpoint");
-        String time = null;
-//        String summary = null;
-//        String link = null;
-        System.out.println(parser.getName());
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            if (name.equals("Time")) {
-                time = readText(parser);
-//            } else if (name.equals("summary")) {
-//                summary = readSummary(parser);
-//            } else if (name.equals("link")) {
-//                link = readLink(parser);
-            } else {
-                skip(parser);
-            }
-        }
-        return new Entry(time);
-    }
+    public List<Trackpoint> parse(InputStream is) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser  parser = factory.newPullParser();
 
-    // Processes link tags in the feed.
-    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
-        String link = "";
-        parser.require(XmlPullParser.START_TAG, ns, "link");
-        String tag = parser.getName();
-        String relType = parser.getAttributeValue(null, "rel");
-        if (tag.equals("link")) {
-            if (relType.equals("alternate")){
-                link = parser.getAttributeValue(null, "href");
-                parser.nextTag();
-            }
-        }
-        parser.require(XmlPullParser.END_TAG, ns, "link");
-        return link;
-    }
+            parser.setInput(is, null);
 
-    // Processes summary tags in the feed.
-    private String readSummary(XmlPullParser parser) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, ns, "summary");
-        String summary = readText(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "summary");
-        return summary;
-    }
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagname = parser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if (tagname.equalsIgnoreCase("Trackpoint")) {
+                            // create a new instance of employee
+                            employee = new Trackpoint();
+                        }
+                        break;
 
-    // For the tags title and summary, extracts their text values.
-    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
-        String result = "";
-        if (parser.next() == XmlPullParser.TEXT) {
-            result = parser.getText();
-            parser.nextTag();
-        }
-        return result;
-    }
-    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-        if (parser.getEventType() != XmlPullParser.START_TAG) {
-            throw new IllegalStateException();
-        }
-        int depth = 1;
-        while (depth != 0) {
-            switch (parser.next()) {
-                case XmlPullParser.END_TAG:
-                    depth--;
-                    break;
-                case XmlPullParser.START_TAG:
-                    depth++;
-                    break;
+                    case XmlPullParser.TEXT:
+                        text = parser.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if (tagname.equalsIgnoreCase("Trackpoint")) {
+                            // add employee object to list
+                            employees.add(employee);
+                        }else if (tagname.equalsIgnoreCase("Time")) {
+                            employee.setId(Integer.parseInt(text));
+                        }  else if (tagname.equalsIgnoreCase("LongitudeDegrees")) {
+                            employee.setName(text);
+                        } else if (tagname.equalsIgnoreCase("LatitudeDegrees")) {
+                            employee.setSalary(Float.parseFloat(text));
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                eventType = parser.next();
             }
-        }
+
+        } catch (XmlPullParserException e) {e.printStackTrace();}
+        catch (IOException e) {e.printStackTrace();}
+
+        return employees;
     }
 }
